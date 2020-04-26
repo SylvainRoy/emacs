9637e5b7ba89cd4c425237d5b29adfc7621af50d
Sylvain Roy <sylvain.roy@m4x.org>
2020-04-26 22:49:28 +0200

    Revampt of init.el. Proper support of elpy.

--- init.el
+++ init.el
@@ -1,56 +1,78 @@
-;;
-;; ==== Emacs config filel ====
-;;
-
-;; Packages to install with this .emacs:
-;;  - ido-vertical
-;;  - ido-ubiquitous
-;;  - multiple-cursors
-;;  - smex
-;;  - pcre2el
-;;  - yasnippet
-;;  - elpy
-;;  - ein
+;; init.el --- Emacs configuration
 
 
-;; == Misc customizations ==
 
-;; Line numbers are good.  Getting column numbering as well is better.
-(column-number-mode t)
+;; install packages
+;; --------------------------------------
 
-;; Position of the vertical scrollbar.
-(set-scroll-bar-mode 'right)
+(require 'package)
+(add-to-list 'package-archives
+       '("melpa" . "https://melpa.org/packages/") t)
 
-; No more tool bar
-(tool-bar-mode -1)
+;; activate all packages
+(package-initialize)
 
-;; Emacs usually has a splash screen on startup.  Let's get rid of
-;; that and start with a blank buffer.
-(setq inhibit-startup-message t)
-(setq initial-scratch-message "")
+;; fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
 
+;; define list of packages to install
+(defvar myPackages
+  '(better-defaults
+    material-theme
+    exec-path-from-shell
+    elpy
+    pyenv-mode
+    multiple-cursors
+    ido-vertical-mode
+    smex
+    pcre2el))
+
+;; install all packages in list
+(mapc #'(lambda (package)
+    (unless (package-installed-p package)
+      (package-install package)))
+      myPackages)
+
+;; Use shell's $PATH
+(exec-path-from-shell-copy-env "PATH")
+
+
+
+;; basic customization
+;; --------------------------------------
+
+(setq inhibit-startup-message t)   ;; hide the startup message
+(setq initial-scratch-message "")  ;; scratch is an empty buffer
+(load-theme 'material t)           ;; load material theme
+(column-number-mode t)             ;; add column number
+(tool-bar-mode -1)                 ;; remove tool bar
 ;; Remove useless whitespace before saving
 (add-hook 'before-save-hook 'whitespace-cleanup)
-
 ;; On Mac, the 'fn' key should behave as the ctrl key
 (setq ns-function-modifier 'control)
 
-;; == Package sources ==
-
-;; Adding more sources of packages
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("marmalade" . "http://marmalade-repo.org/packages/")
-			 ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 
-;; == Ein ==
+;; enable elpy (enhanced python mode)
+;; --------------------------------------
 
-;; memo:
-;;  - Start by "M-X ein:notebook-list-open"
-;;  - then, everyting's in the menus
+;; C-c C-c: eval selection / buffer in (i)python shell
+;; C-c C-z: launch (i)python shell
+;; M-Tab: propose completion
+;;   M-n / M-p: move through the various proposition
+;; C-c C-d: display doc
+;; M-.: goto definition
+
+(elpy-enable)
+(pyenv-mode)
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "-i --simple-prompt")
+(setq elpy-shell-echo-output nil)
 
 
-;; == Multiple Cursors ==
+;; multiple cursors
+;; --------------------------------------
 
 ;; memo:
 ;;  - `mc/insert-numbers`: Insert increasing numbers for each cursor, top to bottom.
@@ -73,16 +95,16 @@
        )))
 
 
-;; == Perl like reg exp ==
-;; Reminder: C-q C-j to insert new line in mini buffer
-(global-set-key [(meta %)] 'pcre-query-replace-regexp)
 
-
-
-;; == Better completion with Ido ==
+;; ido & smex configuration
+;; --------------------------------------
 
 ;; memo:
 ;;  - C-j when enter should not use proposed choice
+;;  - C-f to revert to 'old' find-file
+;;  - C-b to revert to 'old' find-buffer
+;;  - C-s / C-r to go forward/backward in the list of options
+;;  - //, ~/ can be used in ido
 
 (require 'ido)
 (setq ido-enable-flex-matching t)
@@ -92,11 +114,104 @@
 ;; ido display choices vertically
 (add-hook 'ido-setup-hook (lambda () (ido-vertical-mode 1)))
 
-;; wider usage of ido
-(add-hook 'ido-setup-hook (lambda () (ido-ubiquitous-mode 1)))
+;; activate all packages
+(package-initialize)
+
+;; fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; define list of packages to install
+(defvar myPackages
+  '(better-defaults
+    material-theme
+    exec-path-from-shell
+    elpy
+    pyenv-mode
+    multiple-cursors
+    ido-vertical-mode
+    smex
+    pcre2el
+    ))
+
+;; install all packages in list
+(mapc #'(lambda (package)
+    (unless (package-installed-p package)
+      (package-install package)))
+      myPackages)
+
+;; Use shell's $PATH
+(exec-path-from-shell-copy-env "PATH")
 
 
-;; == Smex ==
+
+;; basic customization
+;; --------------------------------------
+
+(setq inhibit-startup-message t)   ;; hide the startup message
+(setq initial-scratch-message "")  ;; scratch is an empty buffer
+(load-theme 'material t)           ;; load material theme
+(column-number-mode t)             ;; add column number
+(tool-bar-mode -1)                 ;; remove tool bar
+;; Remove useless whitespace before saving
+(add-hook 'before-save-hook 'whitespace-cleanup)
+;; On Mac, the 'fn' key should behave as the ctrl key
+(setq ns-function-modifier 'control)
+
+
+
+;; enable elpy (enhanced python mode)
+;; --------------------------------------
+
+(elpy-enable)
+(pyenv-mode)
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "-i --simple-prompt")
+
+
+
+;; multiple cursors
+;; --------------------------------------
+
+;; memo:
+;;  - `mc/insert-numbers`: Insert increasing numbers for each cursor, top to bottom.
+;;  - Sometimes you end up with cursors outside of your view. You can
+;;    scroll the screen to center on each cursor with `C-v` and `M-v`.
+;;  - If you get out of multiple-cursors-mode and yank - it will yank only
+;;    from the kill-ring of main cursor. To yank from the kill-rings of
+;;    every cursor use yank-rectangle, normally found at C-x r y.
+
+(eval-after-load "multiple-cursors-autoloads"
+  '(progn
+     (if (require 'multiple-cursors nil t)
+	 (progn
+	   (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+	   (global-set-key (kbd "C-,") 'mc/mark-next-like-this)
+	   (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+	   (global-set-key (kbd "C-c C-,") 'mc/mark-all-like-this)
+	   )
+       (warn "multiple-cursors was not found.")
+       )))
+
+
+
+;; ido & smex configuration
+;; --------------------------------------
+
+;; memo:
+;;  - C-j when enter should not use proposed choice
+;;  - C-f to revert to 'old' find-file
+;;  - C-b to revert to 'old' find-buffer
+;;  - //, ~/ can be used in ido
+
+(require 'ido)
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+;; ido display choices vertically
+(add-hook 'ido-setup-hook (lambda () (ido-vertical-mode 1)))
+(setq ido-vertical-define-keys 'C-n-and-C-p-only)
 
 (autoload 'smex "smex"
   "Smex is a M-x enhancement for Emacs, it provides a convenient interface to
@@ -105,7 +220,18 @@ your recently and most frequently used commands.")
 (global-set-key (kbd "M-x") 'smex)
 
 
-;; == Join lines ==
+
+;; perl like reg exp
+;; --------------------------------------
+
+;; == Perl like reg exp ==
+;; Reminder: C-q C-j to insert new line in mini buffer
+(global-set-key [(meta %)] 'pcre-query-replace-regexp)
+
+
+
+;; elisp config
+;; --------------------------------------
 
 ;; Key-binding to join lines
 (global-set-key (kbd "M-j")
@@ -113,20 +239,7 @@ your recently and most frequently used commands.")
 		  (interactive)
 		  (join-line -1)))
 
-
-;; == snippets ==
-
-; todo: refine snippets. Use .yas-parents files to have a root directory for all mode for e.g. email, name, etc.
-(setq yas-snippet-dirs '("~/.emacs.d/snippets"))
-(add-hook 'after-init-hook 'init-yasnippet-hook)
-(defun init-yasnippet-hook ()
-  (require 'yasnippet)
-  (yas-global-mode 1)
-  )
-
-
-;; == Evaluate elisp in place ==
-
+;; Evaluate elisp in place
 (defun eval-and-replace ()
   "Replace the preceding sexp with its value."
   (interactive)
@@ -141,7 +254,9 @@ your recently and most frequently used commands.")
 		'eval-and-replace)
 
 
-;; == Better key bindings to change window ==
+
+;; better key bindings to change window
+;; --------------------------------------
 
 (defun frame-bck()
   (interactive)
@@ -151,39 +266,21 @@ your recently and most frequently used commands.")
 (define-key (current-global-map) (kbd "M-S-o") 'frame-bck)
 
 
-;; == Sticky mode ==
+
+;; Sticky mode
+;; --------------------------------------
 
 (define-minor-mode sticky-buffer-mode
   "Make the current window always display this buffer."
   nil " sticky" nil
   (set-window-dedicated-p (selected-window) sticky-buffer-mode))
 
-(global-set-key [pause] 'sticky-buffer-mode)
+(define-key (current-global-map) (kbd "C-S-o") 'sticky-buffer-mode)
 
 
-;; == Auto load mode ==
 
-;; Auto load html mode with .tmpl files
-(setq auto-mode-alist
-      (nconc
-       '(("\\.tmpl$" . html-mode))
-       auto-mode-alist))
-
-
-;; == recent files mode ==
-
-(recentf-mode 1) ; keep a list of recently opened files
-(global-set-key (kbd "<f7>") 'recentf-open-files)
-(recentf-open-files) ; start with recent files screen
-
-
-;; == Activate elpy (a better python mode) ==
-(package-initialize)
-(elpy-enable)
-(elpy-use-ipython)
-
-
-;; == Backup file management ==
+;; backup file management
+;; --------------------------------------
 
 ; Locate all backup files in one place.
 (defvar user-temporary-file-directory "~/.emacs-autosaves/")
@@ -198,20 +295,21 @@ your recently and most frequently used commands.")
       `((".*" ,user-temporary-file-directory t)))
 
 
-;; == Do not edit below this point ==
+
+;; DO NOT EDIT BELOW THIS POINT
+;; --------------------------------------
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(custom-enabled-themes (quote (misterioso)))
- '(show-paren-mode t)
- '(tool-bar-mode nil))
+ '(package-selected-packages
+   (quote
+    (pyenv-mode elpy exec-path-from-shell material-theme better-defaults))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 98 :width normal)))))
+ )
